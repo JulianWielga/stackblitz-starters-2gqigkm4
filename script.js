38b857d2b34b21ba388ca6d82211d251df971dc7
Julian Wielga <j@touk.pl>
2025-12-29 01:01:31 +0100

    socket

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+const tenant = "pink-snakes";
+const topic = "socket";
+const token = "KNvNBaUNRA2OQJG3xlseVkVR";
+const storedId = localStorage.getItem("subscriptionId");
+const url = new URL(
+    `wss://${tenant}-blabber.staging-cloud.nussknacker.io/stream/${topic}`
+);
+if (token) url.searchParams.set("token", token);
+if (storedId) url.searchParams.set("subscriptionId", storedId);
+const ws = new WebSocket(url);
+
+ws.onmessage = (e) => {
+    try {
+        const {data, subscriptionId, type} = JSON.parse(e.data);
+        switch (type) {
+            case "session": {
+                localStorage.setItem("subscriptionId", subscriptionId);
+                return;
+            }
+            case "message": {
+                document.getElementById("test").innerText = data;
+                return;
+            }
+        }
+    } catch (e) {
+    }
+};
