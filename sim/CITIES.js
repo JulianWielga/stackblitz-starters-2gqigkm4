a3bdaea808a525fe429593f1cc7513a600d6e34a
Julian Wielga <j@touk.pl>
2025-12-29 16:54:00 +0100

    one city

--- sim/CITIES.js
+++ sim/CITIES.js
@@ -1,10 +1,10 @@
 export const CITIES = [
     { name: "Warsaw", lat: 52.2297, lng: 21.0122 },
-    { name: "Berlin", lat: 52.52, lng: 13.405 },
-    { name: "Paris", lat: 48.8566, lng: 2.3522 },
-    { name: "Madrid", lat: 40.4168, lng: -3.7038 },
-    { name: "Rome", lat: 41.9028, lng: 12.4964 },
-    { name: "London", lat: 51.5074, lng: -0.1278 },
-    { name: "Prague", lat: 50.0755, lng: 14.4378 },
-    { name: "Vienna", lat: 48.2082, lng: 16.3738 },
+    // { name: "Berlin", lat: 52.52, lng: 13.405 },
+    // { name: "Paris", lat: 48.8566, lng: 2.3522 },
+    // { name: "Madrid", lat: 40.4168, lng: -3.7038 },
+    // { name: "Rome", lat: 41.9028, lng: 12.4964 },
+    // { name: "London", lat: 51.5074, lng: -0.1278 },
+    // { name: "Prague", lat: 50.0755, lng: 14.4378 },
+    // { name: "Vienna", lat: 48.2082, lng: 16.3738 },
 ];
